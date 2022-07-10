8275e135a1eb251ebca523c36377aba727d1c530
marunasv <sobtsova@gmail.com>
2022-07-10 22:09:24 +0300

    Moved HTML to JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="42" />
+    <div class="weather-forecast-temperatues">
+      <span class="weather-forecast-temperature-max">18˚ </span>
+      <span class="weather-forecast-temperature-min">12˚ </span>
+  </div>
+</div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +110,6 @@ fahrenheitLink.addEventListener("click", changeUnitFah);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", changeUnitCel);
+
+search("Kyiv");
+displayForecast();
